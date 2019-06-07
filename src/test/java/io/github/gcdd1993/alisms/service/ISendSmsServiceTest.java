a97357fe4b4ecf56c6671c2ecabd776d931e495f
gaochen <gaochen@maxtropy.com>
2019-06-08 01:25:22 +0800

    项目脱敏

--- src/test/java/io/github/gcdd1993/alisms/service/ISendSmsServiceTest.java
+++ src/test/java/io/github/gcdd1993/alisms/service/ISendSmsServiceTest.java
@@ -17,8 +17,6 @@ import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
-import static org.junit.Assert.*;
-
 /**
  * TODO
  *
@@ -41,43 +39,73 @@ public class ISendSmsServiceTest {
     private static final Integer TEMPLATE_ID = 111;
     private static final String SIGN_NAME = "your sign name";
 
+    /**
+     * 同步发送短信
+     * <p>
+     * 参数1：使用的短信模板ID
+     * 参数2：接收者的手机号，如"17602526129,17602923211"
+     * 参数3：Map，key对应模板中的参数名，value对应值（这里是使用Jackson来序列化）
+     * </p>
+     */
     @Test
     public void sendSync() {
         SmsResponse smsResponse = sendService.sendSync(TEMPLATE_ID, PHONE_NUMBER, MAP);
         Assert.assertTrue(smsResponse.isSuccess());
     }
 
+    /**
+     * 同步发送短信
+     * <p>
+     * 参数1：使用的短信模板ID
+     * 参数2：接收者的手机号，如"17602526129,17602923211"
+     * 参数3：要发送的短信写入值，你可以自己进行json的拼装。注意要进行json的转义，例如："{\"code\":\"112233\"}"
+     * </p>
+     */
     @Test
-    public void sendSync1() throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        SmsResponse smsResponse = sendService.sendSync(TEMPLATE_ID, PHONE_NUMBER, mapper.writeValueAsString(MAP));
+    public void sendSync1() {
+        SmsResponse smsResponse = sendService.sendSync(TEMPLATE_ID, PHONE_NUMBER, "{\"code\":\"112233\"}");
         Assert.assertTrue(smsResponse.isSuccess());
     }
 
+    /**
+     * 同步发送短信
+     * <p>
+     * 参数1：短信签名，适用于同一模板需要有不同短信签名的
+     * 参数2：使用的短信模板ID
+     * 参数3：接收者的手机号，如"17602526129,17602923211"
+     * 参数4：Map，key对应模板中的参数名，value对应值（这里是使用Jackson来序列化）
+     * </p>
+     */
     @Test
     public void sendSync2() {
         SmsResponse smsResponse = sendService.sendSync(SIGN_NAME, TEMPLATE_ID, PHONE_NUMBER, MAP);
         Assert.assertTrue(smsResponse.isSuccess());
     }
 
+    /**
+     * 同步发送短信
+     * <p>
+     * 参数1：短信签名，适用于同一模板需要有不同短信签名的
+     * 参数2：使用的短信模板ID
+     * 参数3：接收者的手机号，如"17602526129,17602923211"
+     * 参数4：要发送的短信写入值，你可以自己进行json的拼装。注意要进行json的转义，例如："{\"code\":\"112233\"}"
+     * </p>
+     */
     @Test
-    public void sendSync3() throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        SmsResponse smsResponse = sendService.sendSync(SIGN_NAME, TEMPLATE_ID, PHONE_NUMBER, mapper.writeValueAsString(MAP));
+    public void sendSync3() {
+        SmsResponse smsResponse = sendService.sendSync(SIGN_NAME, TEMPLATE_ID, PHONE_NUMBER, "{\"code\":\"112233\"}");
         Assert.assertTrue(smsResponse.isSuccess());
     }
 
+    /**
+     * 同步发送短信
+     */
     @Test
     public void sendSync4() {
         SmsRequest smsRequest = new SmsRequest();
         smsRequest.setPhoneNumbers(PHONE_NUMBER);
         smsRequest.setTemplateId(TEMPLATE_ID);
-        Map<String, String> map = new HashMap<>(1);
-        map.put("name", "张三");
-        map.put("project", "xx项目");
-        map.put("machine", "xx设备");
-        map.put("rule", "xx报警规则");
-        smsRequest.setParams(map);
+        smsRequest.setParams(MAP);
         SmsResponse smsResponse = sendService.sendSync(smsRequest);
         Assert.assertTrue(smsResponse.isSuccess());
     }
@@ -85,6 +113,13 @@ public class ISendSmsServiceTest {
     @Test
     public void sendAsync() throws ExecutionException, InterruptedException {
         CompletableFuture<SmsResponse> smsResponse = sendService.sendAsync(TEMPLATE_ID, PHONE_NUMBER, MAP);
+        smsResponse.thenAcceptAsync(sr -> {
+            if (sr.isSuccess()) {
+                System.out.println("发短信成功");
+            } else {
+                System.out.println("发送到消息队列，准备重试此次短信");
+            }
+        });
         Assert.assertTrue(smsResponse.get().isSuccess());
     }
 
